2a711ad97a903fdf4aa18a539b03c6d6023a59ed
Vincent Vega <vincent.vega@polymtl.ca>
2022-10-28 14:14:27 -0400

    correction

--- projet/phase 3/heuristique2.jl
+++ projet/phase 3/heuristique2.jl
@@ -3,7 +3,7 @@ function path_compr!(comp::Comp{T}) where T
     root = comp.root
     l = length(comp.children)
     for i = 1 : l 
-        child = comp.children[i][1]
+        child = comp.children[i].nodes[1]
         if child == root
             comp.children[i].rank = 1
         else
