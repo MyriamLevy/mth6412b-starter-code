7010cb145a303b789e92b63710f7a1d6fd5e5197
Vincent Vega <vincent.vega@polymtl.ca>
2022-10-28 14:11:30 -0400

    compression des chemins

--- projet/phase 3/heuristique2.jl
+++ projet/phase 3/heuristique2.jl
@@ -1,1 +1,15 @@
 #code Heuristique2
+function path_compr!(comp::Comp{T}) where T
+    root = comp.root
+    l = length(comp.children)
+    for i = 1 : l 
+        child = comp.children[i][1]
+        if child == root
+            comp.children[i].rank = 1
+        else
+        comp.children[i] = Child((child, root), 0)
+        end
+    end
+    return comp
+end
+        
