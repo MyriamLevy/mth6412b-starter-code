50b63de9a689cf2e222022def8a4716d278258ea
Vincent Vega <vincent.vega@polymtl.ca>
2022-10-31 11:32:29 -0400

    comp

--- projet/phase2/comp.jl
+++ projet/phase2/comp.jl
@@ -3,14 +3,25 @@ using Test
 """Type abstrait dont d'autres types de composantes connexes dériveront."""
 abstract type AbstractComp{T} end
 
+"""Type abstrait dont d'autres types d'éléments de composantes connexes dériveront."""
+abstract type AbstractChild{T} end
 
-""" C'est une structure de composante connexe qui prend deux arguments:
-        root: qui répresente le noeud qui sert de racine à l'arbre
-        children: est un vecteur qui contients des tuples avec trois éléments. Les deux premiers sont des noeuds de la forme(a,b) ou b est le noeud parent de a. Le troisième élément est le rang du noeud au sein de la composante connexe.
-        """
-mutable struct Comp{T}<:AbstractComp{T} 
+"""Type représentant les éléments d'une composante connexe.
+
+Chaque enfant est un tuple de la forme (enfant, parent) et a un attribut rang."""
+mutable struct Child{T} <: AbstractChild{T}
+    nodes::Tuple{Node{T}, Node{T}} #chaque enfant est un tuple de la forme (enfant, parent)
+    rank::Int
+end
+
+"""Type représentant les composantes connexes d'un graphe.
+
+root est le noeud-racine de la composante et les autres noeuds de la composante, 
+ainsi que le noeud-racine, sont dans children. 
+Chaque noeud est couplé à son parent. root est couplé à lui-même."""
+mutable struct Comp{T} <: AbstractComp{T}
     root::Node{T}
-    children::Vector{Tuple{Node{T},Node{T},Int}}
+    children::Vector{Child{T}} 
 end
 
 """Renvoie la racine de la composante connexe"""
@@ -19,6 +30,11 @@ get_root(comp::AbstractComp) = comp.root
 """Renvoie les noeuds de la composante connexe"""
 get_children(comp::AbstractComp) = comp.children
 
+"""Renvoie le rang d'un élément de la composante connexe"""
+get_rank(child::AbstractChild) = child.rank 
+
+"""Renvoie le tuple (enfant, parent) d'un élément de la composante connexe"""
+get_nodes(child::AbstractChild) = child.nodes
 
 """Fonction permettant de fusionner deux composantes connexes comp1 et comp2.
 
@@ -28,14 +44,16 @@ Ici, on ne se soucie pas du rang des éléments, aucun rang n'est modifié."""
 function merge!(comp1::Comp{T}, comp2::Comp{T}) where T
     r1 = get_root(comp1)
     r2 = get_root(comp2)
+    rang = 0
     for i = 1 : length(comp2.children) 
         r = comp2.children[i]
-        if r[1] != r2 #on ajoute tous les noeuds de comp2 à comp1 à l'exception de (r2, r2) car r1 sera la racine
-           r[1][3]=r[1][3]+1
+        if r.nodes[1] != r2 #on ajoute tous les noeuds de comp2 à comp1 à l'exception de (r2, r2) car r1 sera la racine
             push!(comp1.children, r)
+        else
+            rang = get_rank(r) #on récupère le rang de r2
         end
     end
-    push!(comp1.children,(r2, r1,1) )
+    push!(comp1.children, (Child((r2, r1), rang)))
     return comp1
 end
 
