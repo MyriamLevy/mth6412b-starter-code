b2d4c4a60470378583d5f8d3ee4d74dcb39cf20f
Abdousamad1 <abdou-samad.dicko@polymtl.ca>
2022-11-28 13:43:56 -0500

    update

--- projet/phase 5/notebook.jl
+++ projet/phase 5/notebook.jl
@@ -4,21 +4,25 @@
 using Markdown
 using InteractiveUtils
 
+# ╔═╡ ec313840-ba42-4713-a4a3-bd6d83cfa766
+md"""## Projet du voyageur de commerce : Phase 5"""
+
+# ╔═╡ 706f9a6f-c122-491b-8a8c-e73808f36405
+md"""Abdou Samad Dicko(2037205), Clélia Merel(2163025), Myriam Lévy(2225114)"""
+
 # ╔═╡ 76767d20-6f35-11ed-00c8-a9b6c983afdd
 md"""
-|Originale | Shuffle   	| Reconstructed	|
-|---	|---	|---	|
-| ![Abstract light painting](https://raw.githubusercontent.com/MyriamLevy/mth6412b-starter-code/Phase-5/shredder-julia/images/original/abstract-light-painting.png)|![](LINK GITHUB )  	|   	|
-| ![Alaska Rail Road](LINK GITHUB )|   	|   	|
-| ![Blue hour paris](LINK GITHUB )|   	|   	|
-| ![Lower kananaskis lake](LINK GITHUB )	|   	|   	|
-| ![Marlet2 radio board](LINK GITHUB )|   	|   	|
-| ![Niko's cat](LINK GITHUB )	|   	|   	|
-| ![Pizza food wallpaper](LINK GITHUB )	|   	|   	|
-| ![The enchanted garden](LINK GITHUB )	|   	|   	|
-| ![Tokyo skytree aerial](LINK GITHUB )  	|   	|   	|
-
-
+|Originale | Shuffle   	| Reconstructed	| Longueur de la meilleure tournée	|
+|---	|---	|---	|---	|
+| ![Abstract light painting](https://raw.githubusercontent.com/MyriamLevy/mth6412b-starter-code/Phase-5/shredder-julia/images/original/abstract-light-painting.png)|![Abstract light painting](https://raw.githubusercontent.com/MyriamLevy/mth6412b-starter-code/Phase-5/shredder-julia/images/shuffled/abstract-light-painting.png)|   	|   	|
+| ![Alaska Rail Road](https://raw.githubusercontent.com/MyriamLevy/mth6412b-starter-code/Phase-5/shredder-julia/images/original/alaska-railroad.png)|![Alaska Rail Road](https://raw.githubusercontent.com/MyriamLevy/mth6412b-starter-code/Phase-5/shredder-julia/images/shuffled/alaska-railroad.png)|   	|   	|
+| ![Blue hour paris](https://raw.githubusercontent.com/MyriamLevy/mth6412b-starter-code/Phase-5/shredder-julia/images/original/blue-hour-paris.png)|![Blue hour paris](https://raw.githubusercontent.com/MyriamLevy/mth6412b-starter-code/Phase-5/shredder-julia/images/shuffled/blue-hour-paris.png)|   	|   	|
+| ![Lower kananaskis lake](https://raw.githubusercontent.com/MyriamLevy/mth6412b-starter-code/Phase-5/shredder-julia/images/original/lower-kananaskis-lake.png)	|![Lower kananaskis lake](https://raw.githubusercontent.com/MyriamLevy/mth6412b-starter-code/Phase-5/shredder-julia/images/shuffled/lower-kananaskis-lake.png)	|   	|   	|
+| ![Marlet2 radio board](https://raw.githubusercontent.com/MyriamLevy/mth6412b-starter-code/Phase-5/shredder-julia/images/original/marlet2-radio-board.png)|![Marlet2 radio board](https://raw.githubusercontent.com/MyriamLevy/mth6412b-starter-code/Phase-5/shredder-julia/images/shuffled/marlet2-radio-board.png)|   	|   	|
+| ![Niko's cat](https://raw.githubusercontent.com/MyriamLevy/mth6412b-starter-code/Phase-5/shredder-julia/images/original/nikos-cat.png)	|![Niko's cat](https://raw.githubusercontent.com/MyriamLevy/mth6412b-starter-code/Phase-5/shredder-julia/images/shuffled/nikos-cat.png)|   	|   	|
+| ![Pizza food wallpaper](https://raw.githubusercontent.com/MyriamLevy/mth6412b-starter-code/Phase-5/shredder-julia/images/original/pizza-food-wallpaper.png)	|![Pizza food wallpaper](https://raw.githubusercontent.com/MyriamLevy/mth6412b-starter-code/Phase-5/shredder-julia/images/shuffled/pizza-food-wallpaper.png)|   	|   	|
+| ![The enchanted garden](https://raw.githubusercontent.com/MyriamLevy/mth6412b-starter-code/Phase-5/shredder-julia/images/original/pizza-food-wallpaper.png)	|![The enchanted garden](https://raw.githubusercontent.com/MyriamLevy/mth6412b-starter-code/Phase-5/shredder-julia/images/shuffled/pizza-food-wallpaper.png)|   	|   	|
+| ![Tokyo skytree aerial](https://raw.githubusercontent.com/MyriamLevy/mth6412b-starter-code/Phase-5/shredder-julia/images/original/tokyo-skytree-aerial.png)  	|![Tokyo skytree aerial](https://raw.githubusercontent.com/MyriamLevy/mth6412b-starter-code/Phase-5/shredder-julia/images/shuffled/tokyo-skytree-aerial.png)|   	|   	|
 
 
 """
@@ -40,6 +44,8 @@ project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
 """
 
 # ╔═╡ Cell order:
+# ╠═ec313840-ba42-4713-a4a3-bd6d83cfa766
+# ╠═706f9a6f-c122-491b-8a8c-e73808f36405
 # ╠═76767d20-6f35-11ed-00c8-a9b6c983afdd
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
