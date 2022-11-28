d8bb250711b28b89d1da47d5a8b449a39feeb16e
Abdousamad1 <abdou-samad.dicko@polymtl.ca>
2022-11-28 11:42:54 -0500

    notebook pluto

---
+++ projet/phase 5/notebook.jl
@@ -0,0 +1,45 @@
+### A Pluto.jl notebook ###
+# v0.19.11
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 76767d20-6f35-11ed-00c8-a9b6c983afdd
+md"""
+|Originale | Shuffle   	| Reconstructed	|
+|---	|---	|---	|
+| ![Abstract light painting](LINK GITHUB )|![](LINK GITHUB )  	|   	|
+| ![Alaska Rail Road](LINK GITHUB )|   	|   	|
+| ![Blue hour paris](LINK GITHUB )|   	|   	|
+| ![Lower kananaskis lake](LINK GITHUB )	|   	|   	|
+| ![Marlet2 radio board](LINK GITHUB )|   	|   	|
+| ![Niko's cat](LINK GITHUB )	|   	|   	|
+| ![Pizza food wallpaper](LINK GITHUB )	|   	|   	|
+| ![The enchanted garden](LINK GITHUB )	|   	|   	|
+| ![Tokyo skytree aerial](LINK GITHUB )  	|   	|   	|
+
+
+
+
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.8.0"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═76767d20-6f35-11ed-00c8-a9b6c983afdd
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
