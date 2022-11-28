b83dc4b745565449aedac805117be0066f3ec88d
Vincent Vega <vincent.vega@polymtl.ca>
2022-11-28 13:57:13 -0500

    modifs

---
+++ projet/phase 5/main.jl
@@ -0,0 +1,9 @@
+
+
+make_tour("/Users/myriam/Desktop/Phase 1/shredder-julia/tsp/instances/blue-hour-paris.tsp", "paris.tour")
+make_tour("/Users/myriam/Desktop/Phase 1/shredder-julia/tsp/instances/lower-kananaskis-lake.tsp", "lake.tour")
+make_tour("/Users/myriam/Desktop/Phase 1/shredder-julia/tsp/instances/marlet2-radio-board.tsp", "radio.tour")
+make_tour("/Users/myriam/Desktop/Phase 1/shredder-julia/tsp/instances/nikos-cat.tsp", "cat.tour")
+make_tour("/Users/myriam/Desktop/Phase 1/shredder-julia/tsp/instances/pizza-food-wallpaper.tsp", "pizza.tour")
+make_tour("/Users/myriam/Desktop/Phase 1/shredder-julia/tsp/instances/the-enchanted-garden.tsp", "garden.tsp")
+make_tour("/Users/myriam/Desktop/Phase 1/shredder-julia/tsp/instances/tokyo-skytree-aerial.tsp", "tokyo.tour")
